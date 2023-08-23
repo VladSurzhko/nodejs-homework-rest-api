94978acd9f2f5d2332d9068c1df4597cc05ba003
VladSurzhko <surzhkovlad69@gmail.com>
2023-08-23 21:01:14 +0200

    updateee

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 
 
 mongoose
-  .connect("mongodb+srv://Vlad5311:Vlad5311@cluster0.ctlwk4z.mongodb.net/")
+  .connect("mongodb+srv://Vlad5311:Vlad5311@cluster0.ctlwk4z.mongodb.net/db-contacts?retryWrites=true&w=majority")
   .then(() => {
     console.log("Database connection successful");
     app.listen(3000, () => {
