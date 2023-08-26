be3ced30ea8d8c7da95e10c2bbe5c2096b5a55dc
VladSurzhko <surzhkovlad69@gmail.com>
2023-08-26 09:39:35 +0200

    update error

--- controllers/contacts/add.js
+++ controllers/contacts/add.js
@@ -4,10 +4,15 @@ const { schema } = require("../../schemas/schemaJoi");
 const add = async (req, res) => {
   const body = req.body;
   const { error } = schema.validate(body);
+  
   if (error) {
-    error.status = 400;
-    throw error;
+    const missingField = error.details[0].context.label;
+    const errorMessage = `Missing required ${missingField} field`;
+    const customError = new Error(errorMessage);
+    customError.status = 400;
+    throw customError;
   }
+
   const result = await Contact.create(body);
   res.status(201).json({
     status: "success",
--- controllers/contacts/update.js
+++ controllers/contacts/update.js
@@ -5,9 +5,19 @@ const update = async (req, res) => {
   const { contactId } = req.params;
   const body = req.body;
   const { error } = schema.validate(body);
+  
+  if (!body.name && !body.email && !body.phone) {
+    const missingFieldsError = new Error("missing fields");
+    missingFieldsError.status = 400;
+    throw missingFieldsError;
+  }
+
   if (error) {
-    error.status = 400;
-    throw error;
+    const missingField = error.details[0].context.label;
+    const errorMessage = `Missing required ${missingField} field`;
+    const customError = new Error(errorMessage);
+    customError.status = 400;
+    throw customError;
   }
   const updatedContact = await Contact.findByIdAndUpdate(contactId, body, {
     new: true,
