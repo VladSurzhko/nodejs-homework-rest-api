bbcfd1e81a2cde94948dc17a7dd8deaef7286d6b
VladSurzhko <surzhkovlad69@gmail.com>
2023-08-26 10:00:36 +0200

    update.env

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 
 
 mongoose
-  .connect("mongodb+srv://Vlad5311:Vlad5311@cluster0.ctlwk4z.mongodb.net/db-contacts?retryWrites=true&w=majority")
+  .connect(process.env.DB_HOST)
   .then(() => {
     console.log("Database connection successful");
     app.listen(3000, () => {
