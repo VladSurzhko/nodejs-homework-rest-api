94af22cbcf5b1512243a1bb22d42b99664d9d7ca
VladSurzhko <surzhkovlad69@gmail.com>
2023-09-05 21:47:53 +0200

    update, add

--- controllers/contacts/add.js
+++ controllers/contacts/add.js
@@ -1,21 +1,49 @@
 const Contact = require("../../models/contact");
 const { schema } = require("../../schemas/schemaJoi");
 
+
 const add = async (req, res) => {
+  const { _id } = req.user;
+
   const body = req.body;
   const { error } = schema.validate(body);
   if (error) {
-    const missingField = error.details[0].context.label;
-    const errorMessage = `Missing required ${missingField} field`;
-    const customError = new Error(errorMessage);
-    customError.status = 400;
-    throw customError;
+    error.status = 400;
+    throw error;
   }
-  const result = await Contact.create(body);
-  res.status(201).json(result);
+  
+  const result = await Contact.create({ ...body, owner: _id });
+  res.status(201).json({
+    status: "success",
+    code: "201",
+    data: {
+      result,
+    },
+  });
 };
 
 module.exports = add;
+
+
+
+
+
+
+// const add = async (req, res) => {
+//   const body = req.body;
+//   const { error } = schema.validate(body);
+//   if (error) {
+//     const missingField = error.details[0].context.label;
+//     const errorMessage = `Missing required ${missingField} field`;
+//     const customError = new Error(errorMessage);
+//     customError.status = 400;
+//     throw customError;
+//   }
+//   const result = await Contact.create(body);
+//   res.status(201).json(result);
+// };
+
+// module.exports = add;
 
 
 
--- controllers/contacts/update.js
+++ controllers/contacts/update.js
@@ -1,23 +1,14 @@
 const Contact = require("../../models/contact");
 const { schema } = require("../../schemas/schemaJoi");
 
+
 const update = async (req, res) => {
   const { contactId } = req.params;
   const body = req.body;
   const { error } = schema.validate(body);
-
-  if (!body.name && !body.email && !body.phone) {
-    const missingFieldsError = new Error("missing fields");
-    missingFieldsError.status = 400;
-    throw missingFieldsError;
-  }
-
   if (error) {
-    const missingField = error.details[0].context.label;
-    const errorMessage = `Missing required ${missingField} field`;
-    const customError = new Error(errorMessage);
-    customError.status = 400;
-    throw customError;
+    error.status = 400;
+    throw error;
   }
   const updatedContact = await Contact.findByIdAndUpdate(contactId, body, {
     new: true,
@@ -28,10 +19,8 @@ const update = async (req, res) => {
     throw error;
   }
 
-  res.json(
-    updatedContact,
-  );
+  res.json(updatedContact);
 };
 
-
 module.exports = update;
+
