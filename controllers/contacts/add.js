0efabe5ecced83f9ed78555cafde779d42c31bcf
VladSurzhko <surzhkovlad69@gmail.com>
2023-09-06 11:15:54 +0200

    updat

--- controllers/contacts/add.js
+++ controllers/contacts/add.js
@@ -23,34 +23,3 @@ const add = async (req, res) => {
 };
 
 module.exports = add;
-
-
-
-
-
-
-// const add = async (req, res) => {
-//   const body = req.body;
-//   const { error } = schema.validate(body);
-//   if (error) {
-//     const missingField = error.details[0].context.label;
-//     const errorMessage = `Missing required ${missingField} field`;
-//     const customError = new Error(errorMessage);
-//     customError.status = 400;
-//     throw customError;
-//   }
-//   const result = await Contact.create(body);
-//   res.status(201).json(result);
-// };
-
-// module.exports = add;
-
-
-
-
-
-
-
-
-
-
