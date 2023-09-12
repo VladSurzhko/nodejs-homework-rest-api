ad126d96fc1cb5cd6bf08297b564e1f31b205f12
VladSurzhko <surzhkovlad69@gmail.com>
2023-09-12 19:12:08 +0200

    update

--- helpers/index.js
+++ helpers/index.js
@@ -1,9 +1,11 @@
 const httpError = require("./HttpError");
 const ctrlWrapper = require("./ctrlWrapper");
 const errorMongooseHandler = require("./errorMongooseHandler");
+const sendEmail = require("./sendEmail")
 
 module.exports = {
   httpError,
   ctrlWrapper,
   errorMongooseHandler,
+  sendEmail
 };
